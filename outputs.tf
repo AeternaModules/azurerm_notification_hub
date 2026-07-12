6d22861bf242f689953b36d299b35056d45b6e95
Daniel Allen <dan@aeternagroup.net>
2026-07-12 14:19:29 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,41 +1,36 @@
-output "notification_hubs" {
-  description = "All notification_hub resources"
-  value       = azurerm_notification_hub.notification_hubs
-  sensitive   = true
-}
 output "notification_hubs_apns_credential" {
-  description = "List of apns_credential values across all notification_hubs"
-  value       = [for k, v in azurerm_notification_hub.notification_hubs : v.apns_credential]
+  description = "Map of apns_credential values across all notification_hubs, keyed the same as var.notification_hubs"
+  value       = { for k, v in azurerm_notification_hub.notification_hubs : k => v.apns_credential }
   sensitive   = true
 }
 output "notification_hubs_browser_credential" {
-  description = "List of browser_credential values across all notification_hubs"
-  value       = [for k, v in azurerm_notification_hub.notification_hubs : v.browser_credential]
+  description = "Map of browser_credential values across all notification_hubs, keyed the same as var.notification_hubs"
+  value       = { for k, v in azurerm_notification_hub.notification_hubs : k => v.browser_credential }
   sensitive   = true
 }
 output "notification_hubs_gcm_credential" {
-  description = "List of gcm_credential values across all notification_hubs"
-  value       = [for k, v in azurerm_notification_hub.notification_hubs : v.gcm_credential]
+  description = "Map of gcm_credential values across all notification_hubs, keyed the same as var.notification_hubs"
+  value       = { for k, v in azurerm_notification_hub.notification_hubs : k => v.gcm_credential }
   sensitive   = true
 }
 output "notification_hubs_location" {
-  description = "List of location values across all notification_hubs"
-  value       = [for k, v in azurerm_notification_hub.notification_hubs : v.location]
+  description = "Map of location values across all notification_hubs, keyed the same as var.notification_hubs"
+  value       = { for k, v in azurerm_notification_hub.notification_hubs : k => v.location }
 }
 output "notification_hubs_name" {
-  description = "List of name values across all notification_hubs"
-  value       = [for k, v in azurerm_notification_hub.notification_hubs : v.name]
+  description = "Map of name values across all notification_hubs, keyed the same as var.notification_hubs"
+  value       = { for k, v in azurerm_notification_hub.notification_hubs : k => v.name }
 }
 output "notification_hubs_namespace_name" {
-  description = "List of namespace_name values across all notification_hubs"
-  value       = [for k, v in azurerm_notification_hub.notification_hubs : v.namespace_name]
+  description = "Map of namespace_name values across all notification_hubs, keyed the same as var.notification_hubs"
+  value       = { for k, v in azurerm_notification_hub.notification_hubs : k => v.namespace_name }
 }
 output "notification_hubs_resource_group_name" {
-  description = "List of resource_group_name values across all notification_hubs"
-  value       = [for k, v in azurerm_notification_hub.notification_hubs : v.resource_group_name]
+  description = "Map of resource_group_name values across all notification_hubs, keyed the same as var.notification_hubs"
+  value       = { for k, v in azurerm_notification_hub.notification_hubs : k => v.resource_group_name }
 }
 output "notification_hubs_tags" {
-  description = "List of tags values across all notification_hubs"
-  value       = [for k, v in azurerm_notification_hub.notification_hubs : v.tags]
+  description = "Map of tags values across all notification_hubs, keyed the same as var.notification_hubs"
+  value       = { for k, v in azurerm_notification_hub.notification_hubs : k => v.tags }
 }
 
